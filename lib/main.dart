45a7bc5a57bbad3895b1454ee86566c8f30c96af
navegantes2020 <cristian.olivares@agencialosnavegantes.cl>
2020-03-30 16:47:33 -0300

    Desde Android Studio 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Cristián desde Android Studio 2',
+      title: 'Flutter Demo Cristián desde Android Studio 2020',
       theme: ThemeData(
         // This is the theme of your application.
         //
